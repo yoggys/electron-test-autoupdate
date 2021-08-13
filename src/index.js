8dbddcfc4c9e3aee581f05fcc1f4e0cab2ba07da
yoggys <61660055+yoggys@users.noreply.github.com>
2021-08-13 15:05:48 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
 const path = require('path');
+
+const {autoUpdater} = require('electron-updater');
+const log = require('electron-log');
+
+// configure logging
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
+
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
 }
 
+let mainWindow;
+
 const createWindow = () => {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600, 
     webPreferences: {
@@ -22,6 +33,7 @@ const createWindow = () => {
 
   // Open the DevTools.
   //mainWindow.webContents.openDevTools();
+  autoUpdater.checkForUpdates();
 };
 
 // This method will be called when Electron has finished
@@ -29,12 +41,12 @@ const createWindow = () => {
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
-app.on('ready', () => {
-  require('update-electron-app')({
-    repo: 'yoggys/electron-test-autoupdate',
-    logger: require('electron-log')
-  })
-});
+// app.on('ready', () => {
+//   require('update-electron-app')({
+//     repo: 'yoggys/electron-test-autoupdate',
+//     logger: require('electron-log')
+//   })
+// });
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -58,4 +70,46 @@ app.on('activate', () => {
 
 ipcMain.on('appInfo', (event) => {
   event.sender.send('appInfo', { name: app.getName(), version: app.getVersion()})
+});
+
+const sendStatusToWindow = (text) => {
+  log.info(text);
+  if (mainWindow) {
+    mainWindow.webContents.send('message', text);
+  }
+};
+
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+});
+autoUpdater.on('update-available', (info) => {
+  console.log(info)
+  dialog.showMessageBoxSync(null, {
+    message: `A newer version of the program is available:\n${app.getVersion()} -> ${info.version}`,
+    type: 'info',
+    button: ['Update', 'Later'],
+    title: `${app.getName()} update available`
+  })
+  sendStatusToWindow('Update available.');
+});
+autoUpdater.on('update-not-available', info => {
+  sendStatusToWindow('Update not available.');
+});
+autoUpdater.on('error', err => {
+  sendStatusToWindow(`Error in auto-updater: ${err.toString()}`);
+});
+autoUpdater.on('download-progress', progressObj => {
+  sendStatusToWindow(
+    `Download speed: ${progressObj.bytesPerSecond} - Downloaded ${progressObj.percent}% (${progressObj.transferred} + '/' + ${progressObj.total} + )`
+  );
+});
+autoUpdater.on('update-downloaded', info => {
+  sendStatusToWindow('Update downloaded; will install now');
+});
+
+autoUpdater.on('update-downloaded', info => {
+  // Wait 5 seconds, then quit and install
+  // In your application, you don't need to wait 500 ms.
+  // You could call autoUpdater.quitAndInstall(); immediately
+  autoUpdater.quitAndInstall();
 });
